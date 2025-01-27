712863fadc1539b6a5a711b2b883e107c6e674b0
dr3gos - macbook <for.my.games.email@gmail.com>
2025-01-27 07:57:42 +0200

    pneumatics

--- src/main.py
+++ src/main.py
@@ -20,6 +20,8 @@ brain.screen.print("Hello V5")
 controller_1 = Controller(PRIMARY)
 leftRear = Motor(Ports.PORT3, GearSetting.RATIO_18_1, True)       
 rightRear = Motor(Ports.PORT4, GearSetting.RATIO_18_1, False)
+sucker = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
+pneumatic = DigitalOut(brain.three_wire_port.a)
 
 wait(30, MSEC)
 
@@ -42,6 +44,22 @@ def drivingsimple():
     leftRear.spin(FORWARD)
     rightRear.spin(FORWARD)
 
+def operate_sucker():
+    sucker.set_velocity(100, PERCENT)
+    if controller_1.buttonR2.pressing():
+        sucker.spin(REVERSE)
+        wait(5, MSEC)
+    if controller_1.buttonL2.pressing():
+        sucker.spin(FORWARD)
+        wait(5, MSEC)
+    else:
+        sucker.stop()
+
+def pneumaticControl():
+    if controller_1.buttonA.pressing():
+        pneumatic.set(True)
+    if controller_1.buttonB.pressing():
+        pneumatic.set(False)
 
 # Main loop
 while 1:
@@ -51,6 +69,8 @@ while 1:
     if controller_1.buttonLeft.pressing():
         while 1:
             drivingsimple()
+            operate_sucker()
+            pneumaticControl()
     wait(5, MSEC)
 
     
