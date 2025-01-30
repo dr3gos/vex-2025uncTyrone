c8128442d14cac6fff1e782dc347ca998313e043
dr3gos - macbook <for.my.games.email@gmail.com>
2025-01-30 08:59:12 +0200

    main2 should be massively optimized / overall smarter

--- src/main.py
+++ src/main.py
@@ -20,8 +20,9 @@ brain.screen.print("Hello V5")
 controller_1 = Controller(PRIMARY)
 leftRear = Motor(Ports.PORT3, GearSetting.RATIO_18_1, True)       
 rightRear = Motor(Ports.PORT4, GearSetting.RATIO_18_1, False)
-sucker = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
-pneumatic = DigitalOut(brain.three_wire_port.a)
+spinnerMotor = Motor(Ports.PORT5, GearSetting.RATIO_18_1, False)
+intakeMotor = Motor(Ports.PORT2, GearSetting.RATIO_18_1, False)
+trapMotor = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
 
 wait(30, MSEC)
 
@@ -44,22 +45,41 @@ def drivingsimple():
     leftRear.spin(FORWARD)
     rightRear.spin(FORWARD)
 
-def operate_sucker():
-    sucker.set_velocity(100, PERCENT)
-    if controller_1.buttonR2.pressing():
-        sucker.spin(REVERSE)
-        wait(5, MSEC)
-    if controller_1.buttonL2.pressing():
-        sucker.spin(FORWARD)
-        wait(5, MSEC)
-    else:
-        sucker.stop()
+def trapForward():
+    # if controller_1.buttonR1.pressing():
+    #     power = 100
+    # elif controller_1.buttonL1.pressing():
+    #     power = -100
+    # else:
+    #     power = 0
 
-def pneumaticControl():
-    if controller_1.buttonA.pressing():
-        pneumatic.set(True)
-    if controller_1.buttonB.pressing():
-        pneumatic.set(False)
+    trapMotor.set_velocity(100, PERCENT)
+    trapMotor.spin(FORWARD)
+
+# def trapBackward():
+#     if controller_1.buttonL1.pressing():
+#         trapMotor.set_velocity(100, PERCENT)
+#         trapMotor.spin(REVERSE)
+#     else:
+#         trapMotor.stop()
+    
+
+def trapStop():
+    trapMotor.stop()
+
+def intake(positive, negative):
+    if controller_1.buttonR2.pressing():
+        power = positive
+    elif controller_1.buttonL2.pressing():
+        power = negative
+    else:
+        power = 0
+    intakeMotor.set_velocity(power, PERCENT)
+    spinnerMotor.set_velocity(power, PERCENT)
+    intakeMotor.spin(FORWARD)
+    spinnerMotor.spin(FORWARD)
+
+trapToggle = 0
 
 # Main loop
 while 1:
@@ -69,8 +89,20 @@ while 1:
     if controller_1.buttonLeft.pressing():
         while 1:
             drivingsimple()
-            operate_sucker()
-            pneumaticControl()
+            intake(100, -100)
+            # trapBackward()
+            # controller_1.screen.clear_screen()
+            # controller_1.screen.print(trapToggle)
+            if controller_1.buttonR1.pressing():
+                if trapToggle == 0:
+                    trapForward()
+                elif trapToggle == 1:
+                    trapStop()
+                # break
+                trapToggle = 1 - trapToggle
+            
+
+
     wait(5, MSEC)
 
     
---
+++ src/main2.py
@@ -0,0 +1,139 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main2.py                                                     #
+# 	Project:      AISB VEX 24'-25' Unc Tyrone                                  #
+# 	Author:       Dragos S.                                                    #
+# 	Created:      30/01/2025                                                   #
+# 	Description:  Massive optimizations                                        #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+
+from vex import *
+
+# Initialize hardware components
+brain = Brain()
+controller = Controller()
+
+# Configure motors
+left_motor = Motor(Ports.PORT3, GearSetting.RATIO_18_1, False)
+right_motor = Motor(Ports.PORT4, GearSetting.RATIO_18_1, False)
+intake = Motor(Ports.PORT5, GearSetting.RATIO_18_1, False)
+spinner = Motor(Ports.PORT6, GearSetting.RATIO_18_1, False)
+trapper = Motor(Ports.PORT7, GearSetting.RATIO_18_1, False)
+
+# Global state variables
+intake_spinner_state = 0  # 0=stopped, 1=forward, 2=reverse
+trapper_toggled = False
+last_buttons = {
+    'L2': False,
+    'R2': False,
+    'A': False
+}
+
+# Control modes
+class Mode:
+    DISABLED = 0
+    AUTONOMOUS = 1
+    DRIVER = 2
+
+current_mode = Mode.DISABLED
+
+def drive_control():
+    # Arcade drive implementation
+    forward = -controller.axis3.position()
+    turn = controller.axis1.position()
+    
+    left = forward + turn
+    right = forward - turn
+    
+    # Constrain speeds and set motors
+    left_motor.spin(FORWARD, max(-100, min(left, 100)), PERCENT)
+    right_motor.spin(FORWARD, max(-100, min(right, 100)), PERCENT)
+
+def intake_spinner_control():
+    global intake_spinner_state, last_buttons
+    
+    L2 = controller.buttonL2.pressing()
+    R2 = controller.buttonR2.pressing()
+    
+    # Toggle handling
+    if L2 and not last_buttons['L2']:
+        intake_spinner_state = 1 if intake_spinner_state != 1 else 0
+    if R2 and not last_buttons['R2']:
+        intake_spinner_state = 2 if intake_spinner_state != 2 else 0
+    
+    # State execution
+    if intake_spinner_state == 1:
+        intake.spin(FORWARD, 100, PERCENT)
+        spinner.spin(FORWARD, 100, PERCENT)
+    elif intake_spinner_state == 2:
+        intake.spin(REVERSE, 100, PERCENT)
+        spinner.spin(REVERSE, 100, PERCENT)
+    else:
+        intake.stop()
+        spinner.stop()
+    
+    last_buttons['L2'] = L2
+    last_buttons['R2'] = R2
+
+def trapper_control():
+    global trapper_toggled, last_buttons
+    
+    A = controller.buttonA.pressing()
+    B = controller.buttonB.pressing()
+    
+    if A and not last_buttons['A']:
+        trapper_toggled = not trapper_toggled
+    
+    if B:
+        trapper.spin(REVERSE, 100, PERCENT)
+    elif trapper_toggled:
+        trapper.spin(FORWARD, 100, PERCENT)
+    else:
+        trapper.stop()
+    
+    last_buttons['A'] = A
+
+def autonomous():
+    # !!! Autonomous code goes here !!!
+    pass
+    
+
+def driver_control():
+    drive_control()
+    intake_spinner_control()
+    trapper_control()
+
+def check_mode_switch():
+    global current_mode
+    # Switch to autonomous on left arrow press
+    if controller.buttonLeft.pressing() and current_mode == Mode.DISABLED:
+        current_mode = Mode.AUTONOMOUS
+    # Switch to driver control on right arrow press
+    elif controller.buttonRight.pressing() and current_mode == Mode.DISABLED:
+        current_mode = Mode.DRIVER
+
+def main_loop():
+    global current_mode
+    
+    while True:
+        check_mode_switch()
+        
+        if current_mode == Mode.AUTONOMOUS:
+            autonomous()
+            current_mode = Mode.DISABLED  # Return to disabled after autonomous
+        elif current_mode == Mode.DRIVER:
+            driver_control()
+        else:
+            # Stop all motors in disabled mode
+            left_motor.stop()
+            right_motor.stop()
+            intake.stop()
+            spinner.stop()
+            trapper.stop()
+        
+        wait(20, MSEC)
+
+# Start the program
+main_loop()
